ec6260c93511ba506688bfd764f2de783300c80e
sabi-hawk <miansabby516@gmail.com>
2025-08-30 10:22:30 +0500

    timeline style chnage to make it look infinite

--- libs/design-editor/src/editor/Timeline.tsx
+++ libs/design-editor/src/editor/Timeline.tsx
@@ -383,8 +383,8 @@ const Timeline: FC<TimelineProps> = ({ isVisible, onToggle }) => {
           <div
             css={{
               height: '110px',
-              background: 'rgba(255, 255, 255, 0.05)',
-              border: '1px solid rgba(255, 255, 255, 0.2)',
+              // background: 'rgba(255, 255, 255, 0.05)',
+              // border: '1px solid rgba(255, 255, 255, 0.2)',
               borderRadius: '8px',
               position: 'relative',
               overflow: 'hidden',
